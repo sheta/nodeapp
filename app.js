ea56bce8070de2b150eece406439bf3843921e05
sheta <sheta@stanford.edu>
2013-03-10 14:41:27 -0700

    added css rendering capability

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express= require('express');
 var app = express();
-
-
+app.use("/public", express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('index.jade', {
     locals: {
@@ -9,6 +8,9 @@ app.get('/', function(req, res) {
     }
   });
 });
+
+mongoose = require('mongoose').Mongoose
+db = mongoose.connect('mongodb://localhost/nodepad')
 
 app.listen(3000);
 console.log('Listening on port 3000');
