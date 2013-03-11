6d1a92bf4041bd18df1bc1c2171371efb5929999
sheta <sheta@stanford.edu>
2013-03-10 22:23:04 -0700

    database created

--- app.js
+++ app.js
@@ -10,33 +10,94 @@ app.get('/', function(req, res) {
 });
 
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize('database', 'sheta');
+//var sequelize = new Sequelize('database', 'sheta');
+
+var sequelize = new Sequelize('database', 'sheta', 'mysqlroot', {
+    host: 'localhost',
+    port: 3000,
+    timestamps: false
+});
 
 var User = sequelize.define('User', {
 	userID: Sequelize.INTEGER,
+	userName: Sequelize.TEXT,
 	password: Sequelize.TEXT
-});
+}
+, {
+instanceMethods: {
+    getUserId: function() {
+      return this.userID;
+    }
+  }
+}
+);
 
 var Post = sequelize.define('Post', {
 	userID: Sequelize.INTEGER,
 	postText: Sequelize.TEXT,
 	date: Sequelize.DATE,
 	postID: Sequelize.INTEGER
-});
+}
+, {
+instanceMethods: {
+    getPost: function() {
+      return this.postText;
+    }
+    
+  }
+}
+);
 
 var Comment = sequelize.define('Comment', {
 	postID: Sequelize.INTEGER,
 	commentText: Sequelize.TEXT,
 	date: Sequelize.DATE,
-	commentID: Sequelize.INTEGER
-});
+	commentID: Sequelize.INTEGER } 
+, {
+instanceMethods: {
+    getComment: function() {
+      return this.commentText;
+    }
+   
+  }
+}
+
+);
 
 var Tag = sequelize.define('Tag', {
 	tagID: Sequelize.INTEGER,
 	tagText: Sequelize.TEXT,
 	postID: Sequelize.INTEGER
-});
+}
+, {
+instanceMethods: {
+    getTag: function() {
+      return this.tagText;
+    }
+  }
+}
+);
+
+//Associations
+Post.belongsTo(User);
+Post.hasOne(User);
+User.hasMany(Post);
+Post.hasMany(Comment);
+Comment.belongsTo(User);
+Comment.hasOne(User);
+User.hasMany(Comment);
+Post.hasMany(Tag);
+Tag.hasMany(Post);
+
 
 sequelize.sync();
+
+User.create({ userID: 1, userName: 'ravi', password: 'chinnu'}).success(function(task) {
+  // you can now access the newly created task via the variable task
+});
+// search for attributes
+User.find({ where: {userName: 'ravi'} }).success(function(project) {
+  // project will be the first entry of the Projects table with the title 'aProject' || null
+})
 app.listen(3000);
 console.log('Listening on port 3000');
