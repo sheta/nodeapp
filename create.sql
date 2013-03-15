4c52b3b2590e8865536df6184503329cc6686e88
sheta <sheta@stanford.edu>
2013-03-14 20:04:00 -0700

    added database

--- create.sql
+++ create.sql
@@ -1,3 +1,1 @@
-Create Table Posts(itemID INTEGER PRIMARY KEY, Name Text, Description
-Text, Currently Integer, First_Bid Integer, Number_of_Bids Integer,
-BuyPrice Integer, userID Text, Started Text, Ends Text);
+Create Table Posts(postID INTEGER PRIMARY KEY, postText Text, time Text);
